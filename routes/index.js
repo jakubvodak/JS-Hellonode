662ab7ab1755f432e9fc057aa16270dbd9e55b07
jakubvodak <jakub.vodak@ulikeit.com>
2015-07-04 11:00:43 +0200

    user

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,32 @@ router.get('/hello', function(req, res, next) {
     res.send(data);
 });
 
+router.post('/user', function(req, res, next) {
+
+    req.checkBody('name', 'Name cannot be empty').notEmpty();
+
+    req.checkBody('age', 'Age cannot be text').isInt();
+
+    var valErrors = req.validationErrors();
+
+    if (valErrors) {
+      var errorData = {
+        error:"InvalidRequest",
+        message:valErrors
+      };
+
+      return res.send(errorData);
+    };
+
+    //save to database
+
+    var profile = {
+      name: req.body.name,
+      age: req.body.age
+    };
+
+    res.send(profile);
+});
+
+
 module.exports = router;
